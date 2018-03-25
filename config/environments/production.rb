e72aa8cfe7f05566cb3b758c81167d769ffd138b
vongani <vonganikhulufelo@gmail.com>
2018-03-26 12:37:00 +1400

    email link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,9 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
   config.action_mailer.default_url_options = { host: "https://hospitalfilesystem.herokuapp.com" }
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',

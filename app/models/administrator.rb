37317ce1004d8e69ebadb6427e184685d594062e
vongani <vonganikhulufelo@gmail.com>
2018-03-27 01:06:40 +1400

    final

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -10,6 +10,8 @@ validates :fname, :lname,:address1,:address2, :address4, :title, presence: true
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 validates :phone, presence: true, format: { with: regex, :multiline => true }
+regpas = /^(?=.*\d)(?=.*([a-z]||[A-Z]))([\x20-\x7E]){6,40}$/
+  validates :password, format: {with: regpas, :multiline => true, message: "must be at least 6 characters and include one number and one letter and one special character."}
 
   def addaddress
  	 self.address = [self.address1, self.address2, self.address3, self.address4].compact.join(', ')
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -13,6 +13,8 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
                     uniqueness: { case_sensitive: false }
 validates :phone, presence: true, format: { with: regex, :multiline => true }
 validates :fname, :lname,:address1,:address2, :address4, :title, :specialisation, presence: true
+regpas = /^(?=.*\d)(?=.*([a-z]||[A-Z]))([\x20-\x7E]){6,40}$/
+  validates :password, format: {with: regpas, :multiline => true, message: "must be at least 6 characters and include one number and one letter and one special character."}
   def addaddress
  	 self.address = [self.address1, self.address2, self.address3, self.address4].compact.join(', ')
  	 self.role = "doctor"
